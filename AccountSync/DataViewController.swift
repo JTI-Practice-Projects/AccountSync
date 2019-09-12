d205ffb4b111ce974a80f36d90441488eeecc8c8
Trevor Jarvis <tmjarv@gmail.com>
2019-09-12 15:50:24 -0600

    Initial Commit

---
+++ AccountSync/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  AccountSync
+//
+//  Created by Trevor Jarvis on 5/19/19.
+//  Copyright © 2019 Trevor Jarvis. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
